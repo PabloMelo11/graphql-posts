dbf435509731dc58ef66ea00a45fee982e44a469
PabloMelo <pablomelodev@gmail.com>
2021-02-15 12:19:06 -0300

    feat: add apollo server express

--- src/startServer.js
+++ src/startServer.js
@@ -1,4 +1,5 @@
-import { ApolloServer, PubSub } from 'apollo-server';
+import express from 'express';
+import { ApolloServer, PubSub } from 'apollo-server-express';
 import mongoose from 'mongoose';
 
 function startServer({ typeDefs, resolvers }) {
@@ -7,10 +8,18 @@ function startServer({ typeDefs, resolvers }) {
     useUnifiedTopology: true,
   });
 
+  const app = express();
+  const path = '/graphql';
+
   const pubsub = new PubSub();
 
   const server = new ApolloServer({ typeDefs, resolvers, context: { pubsub } });
-  server.listen().then(({ url }) => console.log(`Server started at ${url}`));
+
+  server.applyMiddleware({ app, path });
+
+  app.listen(4000, () => {
+    console.log('Server is running');
+  });
 }
 
 export default startServer;
