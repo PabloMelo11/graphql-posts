65c82dd1281aeb515c12b7df9cae33cceb10502c
PabloMelo <pablomelodev@gmail.com>
2021-02-15 18:55:43 -0300

    feat: add middleware in mutations post

--- src/graphql/modules/posts/resolvers.js
+++ src/graphql/modules/posts/resolvers.js
@@ -3,17 +3,43 @@ import User from '../../../models/User';
 
 export default {
   Post: {
-    author: (post) => User.findById(post.author)
+    author: (post) => User.findById(post.author),
   },
 
   Query: {
     posts: () => Post.find(),
-    post: (_, { id }) => Post.findById(id)
+    post: (_, { id }) => Post.findById(id),
   },
 
   Mutation: {
-    createPost: (_, { data }) => Post.create(data),
-    updatePost: (_, { id, data }) => Post.findOneAndUpdate(id, data, { new: true }),
-    deletePost: async (_, { id }) => !!(await Post.findOneAndDelete(id)),
-  }
-}
+    createPost: async (_, { data }, { req }) => {
+      if (!req.isAuth) {
+        throw new Error('JWT token is missing');
+      }
+
+      const post = await Post.create(data);
+
+      return post;
+    },
+
+    updatePost: async (_, { id, data }, { req }) => {
+      if (!req.isAuth) {
+        throw new Error('JWT token is missing');
+      }
+
+      const post = await Post.findAndModify(id, data, { new: true });
+
+      return post;
+    },
+
+    deletePost: async (_, { id }, { req }) => {
+      if (!req.isAuth) {
+        throw new Error('JWT token is missing');
+      }
+
+      const post = await Post.findOneAndDelete(id);
+
+      return !!post;
+    },
+  },
+};
--- src/startServer.js
+++ src/startServer.js
@@ -2,6 +2,8 @@ import express from 'express';
 import { ApolloServer, PubSub } from 'apollo-server-express';
 import mongoose from 'mongoose';
 
+import isAuth from './middlewares/is-auth';
+
 function startServer({ typeDefs, resolvers }) {
   mongoose.connect('mongodb://localhost:27017/graphql', {
     useNewUrlParser: true,
@@ -9,13 +11,21 @@ function startServer({ typeDefs, resolvers }) {
   });
 
   const app = express();
-  const path = '/graphql';
+
+  app.use(isAuth);
 
   const pubsub = new PubSub();
 
-  const server = new ApolloServer({ typeDefs, resolvers, context: { pubsub } });
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: ({ req }) => ({
+      req,
+      pubsub,
+    }),
+  });
 
-  server.applyMiddleware({ app, path });
+  server.applyMiddleware({ app });
 
   app.listen(4000, () => {
     console.log('Server is running');
